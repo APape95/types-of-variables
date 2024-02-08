11a6d121740e086c2471fd255aaaa4c97a3669fb
Amy Pape <A-C.1995@outlook.com>
2024-02-08 16:31:42 +0000

    orig

---
+++ conversion.py
@@ -0,0 +1,31 @@
+# Asign variables 
+    # num1 = 99.23
+    # num2 = 23
+    # num3 = 150
+    # string1 = "100"
+# Convert num1 into an integer and print this
+# Convert num2 into a float and print this
+# Convert num3 into a string and print this
+# Convert string1 into an integer and print this
+
+# The different variables given
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# Printing num1 as integer removes desimal-intergers are whole number's
+num4 = int(num1)
+print(num4)
+
+# Printing num2 as a float will add .0 on the end as floats have desimal places
+num5 = float(num2)
+print(num5)
+
+# Tunring num3 to string prints 150-the code see as characters
+string2 = str(num3)
+print(string2)
+
+# Print string1 to an integer-the code sees as the numbers not characters
+num6 = int(string1)
+print(num6)
